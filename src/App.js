afa5789cd0dbc4ccf72bf20fdff593dc4e93fac4
Consue <c.anguitasalinas@gmail.com>
2022-08-18 22:13:37 -0400

    feat: page assembly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import Header from './layouts/header/Header';
+import Card from './components/card/Card';
+import Footer from './layouts/footer/Footer';
+import Container from './layouts/container/Container';
+import file from './assets/data.json';
 
-function App() {
+const data = file.data;
+
+function App() {  
   return (
-    
+    <>
+      <Header title="Galería de Imágenes con React" />
+      <Container>
+        <Card info={data[0]} />
+        <Card info={data[1]} />
+        <Card info={data[2]} />
+        <Card info={data[3]} />
+        <Card info={data[4]} />
+      </Container>
+      <Footer />
+    </>
   );
 }
 
